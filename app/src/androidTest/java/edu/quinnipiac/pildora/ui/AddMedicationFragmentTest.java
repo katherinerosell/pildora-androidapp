df55ae19390dc69f972c90cc4fe8817d276e889c
KatieRosell <32073902+KatieRosell@users.noreply.github.com>
2020-04-30 09:49:40 -0400

    Edits to AddMedTest Class
    deleting somethings - the INTENT and its imports

--- app/src/androidTest/java/edu/quinnipiac/pildora/ui/AddMedicationFragmentTest.java
+++ app/src/androidTest/java/edu/quinnipiac/pildora/ui/AddMedicationFragmentTest.java
@@ -1,8 +1,5 @@
 package edu.quinnipiac.pildora.ui;
 
-import android.content.Intent;
-
-import androidx.test.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
@@ -16,6 +13,7 @@ import edu.quinnipiac.pildora.R;
 
 import static androidx.test.espresso.Espresso.closeSoftKeyboard;
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
@@ -25,33 +23,42 @@ public class AddMedicationFragmentTest {
     @Rule
     public ActivityTestRule<MainActivity> myActivityTestRule = new ActivityTestRule<>(MainActivity.class, false, false);
 
-    private static final Intent MY_ACTIVITY_INTENT = new Intent(InstrumentationRegistry.getTargetContext(), MainActivity.class);
+    //private static final Intent MY_ACTIVITY_INTENT = new Intent(getTargetContext(), MainActivity.class);
     @Before
     public void setUp() throws Exception {
-        myActivityTestRule.launchActivity(MY_ACTIVITY_INTENT);
+        //myActivityTestRule.launchActivity(MY_ACTIVITY_INTENT);
     }
 
+    /**
+     * Attach the values to the EditText objects
+     * with respective values.
+     */
     @Test
     public void onStart(){
-        AddMedicationFragment fragment = new AddMedicationFragment();
+        //AddMedicationFragment fragment = new AddMedicationFragment();
         //fragment.getView();//?
         //myActivityTestRule.getActivity().getSupportFragmentManager().beginTransaction().add(R.id.med_name, fragment).commit();
 
+        onView(withId(R.id.med_name)).perform(typeText("Hocus Pocus Motrin"));
         //onView(withId(R.id.nav_host_fragment)).check(matches((isDisplayed())));
         //onView(withId(R.id.med_name)).check(matches((isDisplayed())));
-
-        onView(withId(R.id.med_name)).perform(typeText("Hocus Pocus Motrin"));
         onView(withId(R.id.med_dosage)).perform(typeText("6000"));
         onView(withId(R.id.med_qty)).perform(typeText("1"));
         onView(withId(R.id.med_whenTaken)).perform(typeText("Take 1 every other day for 3 weeks."));
         closeSoftKeyboard();
     }
 
+    /**
+     * Perform the save action
+     */
     @Test
     public void onClick() {
-
+        onView(withId(R.id.button_savePrescription)).perform(click());
     }
 
+    /**
+     * Check if the database actually saved the prescription information!
+     */
     @Test
     public void savePrescription() {
 
--- app/src/main/java/edu/quinnipiac/pildora/testclasses/AddPrescriptionsTest.java
+++ app/src/main/java/edu/quinnipiac/pildora/testclasses/AddPrescriptionsTest.java
@@ -1,17 +1,17 @@
 package edu.quinnipiac.pildora.testclasses;
 
 import android.content.ContentValues;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
+
 import androidx.fragment.app.Fragment;
+
 import edu.quinnipiac.pildora.PildoraDatabaseHelper;
 import edu.quinnipiac.pildora.Prescription;
 import edu.quinnipiac.pildora.R;
@@ -31,19 +31,11 @@ public class AddPrescriptionsTest extends Fragment {
 
     //Prescriptions I will add
     private static Prescription prescriptionD;
-    private static Prescription prescriptionE;
-    private static Prescription prescriptionG;
-    //Database entities needed
-    //private static Prescription _prescription;
     private static SQLiteDatabase db;
-    //private PildoraDatabaseHelper pildoraDBHelper;
-    //private Cursor cursor;
     private static TestPrescription testPrescription;
 
     public AddPrescriptionsTest(SQLiteDatabase theDB){
         prescriptionD = new Prescription("MedicationD", "2.0", "2", "Take 2 every night for 4 months");
-        //prescriptionE = new Prescription("MedicationE", "400", "1", "Take 1 every night with food for 4 weeks");
-        //prescriptionG = new Prescription("MedicationG", "1000", "1", "Take 1 every day for a week");
         db = theDB;
         testPrescription = new TestPrescription();
     }
@@ -61,40 +53,17 @@ public class AddPrescriptionsTest extends Fragment {
         testDVal.put("DOSAGE", prescriptionD.getDosage());
         testDVal.put("QTY", prescriptionD.getQuantity());
         testDVal.put("TIMETAKEN", prescriptionD.getWhenToTake());
-        /**
-        ContentValues testEVal = new ContentValues();
-        testEVal.put("NAME", prescriptionE.getName());
-        testEVal.put("DOSAGE", prescriptionE.getDosage());
-        testEVal.put("QTY", prescriptionE.getQuantity());
-        testEVal.put("TIMETAKEN", prescriptionE.getWhenToTake());
-
-        ContentValues testGVal = new ContentValues();
-        testGVal.put("NAME", prescriptionG.getName());
-        testGVal.put("DOSAGE", prescriptionG.getDosage());
-        testGVal.put("QTY", prescriptionG.getQuantity());
-        testGVal.put("TIMETAKEN", prescriptionG.getWhenToTake());
-         **/
 
         SQLiteOpenHelper pildoraDBHelper = new PildoraDatabaseHelper(this.getContext());
 
         try{
-            //db = pildoraDBHelper.getWritableDatabase();
-            //insert 3 new content values of the prescription objects
             db.insert("MEDS", null, testDVal);
-            //db.insert("MEDS", null, testEVal);
-            //db.insert("MEDS", null, testGVal);
             pildoraDBHelper.onUpgrade(db, db.getVersion(), 3);
             db.close();
         } catch (SQLiteException e){
             Toast toast = Toast.makeText(this.getLayoutInflater().getContext(), "TEST FAILED - UNREACHABLE DATABASE", Toast.LENGTH_LONG);
             toast.show();
         }
-
-        //Log.d("---------", "----------   Print Added Prescriptions + Attributes   --------");
-        //Log.d(" Prescription D:  ", "Name: " + prescriptionD.getName()+ " Dosage: " + prescriptionD.getDosage()+ " Quantity: " + prescriptionD.getQuantity()+ " When Taken:" + prescriptionD.getWhenToTake());
-        //Log.d(" Prescription E:  ", "Name: " + prescriptionE.getName()+ " Dosage: " + prescriptionE.getDosage()+ " Quantity: " + prescriptionE.getQuantity()+ " When Taken:" + prescriptionE.getWhenToTake());
-        //Log.d(" Prescription G:  ", "Name: " + prescriptionG.getName()+ " Dosage: " + prescriptionG.getDosage()+ " Quantity: " + prescriptionG.getQuantity()+ " When Taken:" + prescriptionG.getWhenToTake());
-        //testPrescription.runPrescriptionTest();
     }
 
 
